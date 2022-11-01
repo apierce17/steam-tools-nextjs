99120d6493de6c923abc14f2bef412cae1325419
Ashton Pierce <coalpierce@gmail.com>
2022-11-01 00:22:53 -0500

    Add Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 import { NextSeo } from "next-seo";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
+      <Analytics />
       <NextSeo
         titleTemplate="%s - Steam Tools"
         description="A place with cool features for Steam! Find mutual games with friends on Steam for starters!"
